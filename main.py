0bd3619ff08ae710f027b3dac6ee446b6caf791f
kush2022 <felixkuria12@gmail.com>
2023-11-01 15:54:46 +0300

    First commit

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy.orm import Session
+from blog import schemas, models
+from blog.database import engine, get_db
+from typing import List
+from blog.hashing import Hash
+from blog.routers import blog, user, authentication
 
 
 app = FastAPI()
 
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
 
-@app.get('/')
-async def index():
-    return {"name": "FastAPI"}
 
+
+models.Base.metadata.create_all(engine)
+
+
+
+# -- create user 
